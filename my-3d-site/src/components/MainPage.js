4b920f4ae0ee0ce6338b8aada5d597501aeb9f13
IveryChen <ivy_chen1@brown.edu>
2024-07-15 15:56:32 -0400

    preventing h scrolling

--- my-3d-site/src/components/MainPage.js
+++ my-3d-site/src/components/MainPage.js
@@ -1,106 +1,10 @@
-// import React, {Suspense, useRef, useEffect, useState } from 'react';
-// import * as THREE from 'three';
-// import { Canvas, useLoader, useFrame } from '@react-three/fiber';
-// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-// import { ShaderMaterial } from 'three';
-// import { Environment, OrbitControls, Mesh, Preload, useGLTF } from '@react-three/drei';
 import Card from './Card';
 import React from 'react';
 import jsonData from '../data';
 import images from '../imageImports'; 
 import { Link } from 'react-router-dom';
 
-// const ChromeShader = {
-//   uniforms: {
-//     envMap: { value: null },
-//     roughness: { value: 0.0 },
-//     metalness: { value: 1.0 }
-//   },
-//   vertexShader: `
-//     varying vec3 vReflect;
-//     void main() {
-//       vec4 worldPosition = modelMatrix * vec4(position, 1.0);
-//       vec4 mvPosition = viewMatrix * worldPosition;
-//       vec3 cameraToVertex = normalize(worldPosition.xyz - cameraPosition);
-//       vec3 worldNormal = normalize(normalMatrix * normal);
-//       vReflect = reflect(cameraToVertex, worldNormal);
-//       gl_Position = projectionMatrix * mvPosition;
-//     }
-//   `,
-
-//   fragmentShader: `
-//     uniform samplerCube envMap;
-//     uniform float roughness;
-//     uniform float metalness;
-//     varying vec3 vReflect;
-//     void main() {
-//       vec3 envColor = textureCube(envMap, vReflect).rgb;
-//       gl_FragColor = vec4(envColor, 1.0);
-//     }
-//   `
-// };
-
-// const Model = ({ objPath, envMap, position }) => {
-//   const modelRef = useRef();
-
-//   useEffect(() => {
-//     console.log("Loading OBJ file from:", process.env.PUBLIC_URL + objPath);
-//     const loader = new OBJLoader();
-//     loader.load(
-//       process.env.PUBLIC_URL + objPath,
-//       (obj) => {
-//         if (obj.children && obj.children.length > 0) {
-//           console.log("OBJ file loaded successfully:", objPath);
-
-//           const standardMaterial = new THREE.MeshStandardMaterial({
-//             color: 'white', // Change this to match the cube's color if needed
-//             metalness: 1.0,
-//             roughness: 0.0,
-//             envMapIntensity: 1.0
-//           });
-
-
-//           const chromeMaterial = new ShaderMaterial({
-//             uniforms: {
-//               ...ChromeShader.uniforms,
-//               envMap: { value: envMap }
-//             },
-//             vertexShader: ChromeShader.vertexShader,
-//             fragmentShader: ChromeShader.fragmentShader
-//           });
-
-//           obj.traverse((child) => {
-//             if (child.isMesh) {
-//               child.material = standardMaterial;
-//               child.material.needsUpdate = true;
-//             }
-//           });
-
-//           modelRef.current = obj;
-//         } else {
-//           console.log("No children found in OBJ file:", objPath);
-//         }
-//       },
-//       (xhr) => {
-//         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-//       },
-//       (error) => {
-//         console.error('Error loading OBJ file:', error);
-//       }
-//     );
-//   }, [objPath, envMap]);
-
-//   return modelRef.current ? <primitive object={modelRef.current} scale={10} position={position} /> : null;
-// };
-
 const MainPage = () => {
-
-  // const envMapPath = 'my-3d-site/src/assets/cubemap/';
-  // const envMap = new THREE.CubeTextureLoader().setPath(envMapPath).load([
-  //   'px.jpg', 'nx.jpg',
-  //   'py.jpg', 'ny.jpg',
-  //   'pz.jpg', 'nz.jpg'
-  // ]);
 
   return (
     <div className="main-page">
@@ -112,8 +16,6 @@ const MainPage = () => {
           <div className="graphic-text-container">
             <img src={images.intro} alt="Intro image"></img>
           </div>
-          {/* <img src={images.lookiveryintro} alt="lookivery in custom balloon render"></img> */}
-          {/* <img src={images.lookiveryintro} alt="lookivery in custom balloon render"></img> */}
         </div>
        
       </div>
@@ -137,23 +39,6 @@ const MainPage = () => {
         ))}
       </div>
 
-      {/* <Canvas className="canvas-container"> */}
-        {/* <ambientLight intensity={0.5} /> */}
-        {/* <directionalLight position={[10, 10, 5]} intensity={1.5} /> */}
-        {/* <Model objPath="lookivery_highres.obj" envMap={envMap} position={[3, 0, 0]}/> */}
-        {/* <Model objPath="Easter_bunny.obj" envMap={envMap} position={[3, 0, 0]}/> */}
-
-        {/* <mesh position={[2, 0, 0]}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="orange" />
-        </mesh> */}
-        {/* <OrbitControls /> */}
-        {/* <Environment preset="sunset" /> */}
-        {/* <Suspense> */}
-        {/* <Cars/> */}
-        {/* </Suspense> */}
-        {/* <Preload all /> */}
-      {/* </Canvas> */}
     </div>
   );
 };
