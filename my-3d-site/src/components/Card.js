3e70dfb97a740b135556e8e42220594bb4a20c64
Ivery Chen <ivchen@tesla.com>
2024-08-17 10:43:02 -0700

    modifying card and making roundedimage component

--- my-3d-site/src/components/Card.js
+++ my-3d-site/src/components/Card.js
@@ -1,34 +1,53 @@
+import styled from '@emotion/styled';
 import React from 'react';
-import categoryColours from '../categoryColours'; 
-// import './Card.css'; 
 import { Link } from 'react-router-dom';
 
+import categoryColours from '../categoryColours';
+import RoundedImage from './RoundedImage';
 
-function Card({ path, image, eventName, description, categories }) {
-  return (
-    // <a href={href} className="link">
+const StyledRoundedImage = styled(RoundedImage)`
+  @media (min-width: 768px) {
+    height: auto;
+    margin-bottom: 10px;
+  }
+`;
+
+export default class Card extends React.PureComponent {
+  render() {
+    const { path, image, eventName, description, categories } = this.props;
+
+    return (
       <div className="card">
         <Link to={`/project/${path}`}>
-        <img src={image} alt={eventName} className="rounded-image" />
-        <div className="card-content">
-          <p className="event-name">{eventName}</p>
-          <div className="description-textbox">
-            <p className="description">{description}</p>
+          <StyledRoundedImage
+            image={image}
+            eventName={eventName}
+            width="100%"
+            height="65%"
+            border="1.5px"
+            borderstyle="solid"
+            bordercolor="black"
+          />
+          <div className="card-content">
+            <div className="event-name">{eventName}</div>
+            <div className="description-textbox">
+              <div className="description">{description}</div>
+            </div>
+            <div className="categories">
+              {categories.map((category, index) => (
+                <div 
+                  key={index}
+                  className="category"
+                  style={{ backgroundColor: categoryColours[category] || "#FFFFFF" }}>
+                  <span className="category-text">{category}</span>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="categories">
-            {categories.map((category, index) => (
-              <div key={index}
-              className="category" 
-              style={{ backgroundColor: categoryColours[category] || "#FFFFFF" }}>
-                <span className="category-text">{category}</span>
-              </div>
-            ))}
-          </div>
-        </div>
         </Link>
-      </div>
-      // </a>
-  );
+      </div >
+
+    );
+  }
 }
 
-export default Card;
---
+++ my-3d-site/src/components/RoundedImage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class RoundedImage extends React.PureComponent {
+    render(){
+        const { image, eventName, width, height, border, borderstyle, bordercolor } = this.props;
+
+        return (
+            <img
+            src={image}
+            alt={eventName}
+            width={width}
+            height={height}
+            border={border}
+            borderStyle={borderstyle}
+            bordercolor={bordercolor}
+          />
+        )
+    }
+}
