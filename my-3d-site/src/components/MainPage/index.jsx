5575ac9b472781042f051b5b52375abbe0fe3442
IveryChen <ivy_chen1@brown.edu>
2024-08-17 23:34:27 -0700

    swapping order

--- my-3d-site/src/components/MainPage/index.jsx
+++ my-3d-site/src/components/MainPage/index.jsx
@@ -9,19 +9,18 @@ const config = [
   "Monday_clothes",
   "Monday_raccoon",
   "Monday_duck_2",
-
   "Marathon_ring",
   "Monday_garden",
-  "Marathon_surprise",
-  "Monday_subway_2",
-
-  "Marathon_switch",
   "Monday_garden_2",
+  "Monday_subway_2",
+  "Marathon_switch",
+  "Marathon_surprise",
   "Monday_corridor_2",
+  "Monday_bus_1",
+  "Monday_kid",
   "Marathon_threesteps",
   "Marathon_rock",
-  "Monday_kid",
-  "Monday_bus_1",
+
   // "Monday_subway_1",
 ];
 
