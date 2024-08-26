8f6b887201c2c17280ae27c031160ded38f9815e
IveryChen <ivy_chen1@brown.edu>
2024-08-25 18:15:17 -0700

    dried out card component

--- my-3d-site/src/components/Card/index.jsx
+++ my-3d-site/src/components/Card/index.jsx
@@ -1,18 +1,7 @@
-import styled from "@emotion/styled";
 import React from "react";
-import { Link } from "react-router-dom";
 import rough from "roughjs";
 
-import categoryColours from "../../categoryColours";
-import RoundedImage from "./RoundedImage";
 import "./Card.css";
-
-const StyledRoundedImage = styled(RoundedImage)`
-  @media (min-width: 768px) {
-    height: auto;
-    margin-bottom: 10px;
-  }
-`;
 
 export default class Card extends React.PureComponent {
   constructor(props) {
@@ -44,43 +33,12 @@ export default class Card extends React.PureComponent {
   }
 
   render() {
-    const { path, image, eventName, description, categories } = this.props;
+    const { children } = this.props;
 
     return (
       <div className="card-wrapper">
         <canvas ref={this.canvasRef} className="canvas" />
-        <div className="card">
-          <Link to={`/project/${path}`}>
-            <StyledRoundedImage
-              image={image}
-              eventName={eventName}
-              width="100%"
-              height="65%"
-              border="1.5px"
-              borderstyle="solid"
-              bordercolor="black"
-            />
-            <div className="card-content">
-              <div className="event-name">{eventName}</div>
-              <div className="description-textbox">
-                <div className="description">{description}</div>
-              </div>
-              <div className="categories">
-                {categories.map((category, index) => (
-                  <div
-                    key={index}
-                    className="category"
-                    style={{
-                      backgroundColor: categoryColours[category] || "#FFFFFF",
-                    }}
-                  >
-                    <span className="category-text">{category}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </Link>
-        </div>
+        {children}
       </div>
     );
   }
--- my-3d-site/src/components/CodePage/index.jsx
+++ my-3d-site/src/components/CodePage/index.jsx
@@ -1,11 +1,21 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { Link } from "react-router-dom";
 
 import jsonData from "../../data";
+import categoryColours from "../../categoryColours";
 import images from "../../imageImports";
 import Card from "../../components/Card";
-import "./CodePage.css";
 
+import "./CodePage.css";
+import RoundedImage from "./RoundedImage";
+
+const StyledRoundedImage = styled(RoundedImage)`
+  @media (min-width: 768px) {
+    height: auto;
+    margin-bottom: 10px;
+  }
+`;
 const CodePage = () => {
   return (
     <>
@@ -22,7 +32,41 @@ const CodePage = () => {
               eventName={item.eventName}
               description={item.description}
               categories={item.categories}
-            />
+            >
+              <div className="card">
+                <Link to={`/project/${item.path}`}>
+                  <StyledRoundedImage
+                    image={images[item.image]}
+                    eventName={item.eventName}
+                    width="100%"
+                    height="65%"
+                    border="1.5px"
+                    borderstyle="solid"
+                    bordercolor="black"
+                  />
+                  <div className="card-content">
+                    <div className="event-name">{item.eventName}</div>
+                    <div className="description-textbox">
+                      <div className="description">{item.description}</div>
+                    </div>
+                    <div className="categories">
+                      {item.categories.map((category, index) => (
+                        <div
+                          key={index}
+                          className="category"
+                          style={{
+                            backgroundColor:
+                              categoryColours[category] || "#FFFFFF",
+                          }}
+                        >
+                          <span className="category-text">{category}</span>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </Card>
           </Link>
         ))}
       </div>
