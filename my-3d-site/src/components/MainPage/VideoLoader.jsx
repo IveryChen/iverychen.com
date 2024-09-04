8c927a35c272fe245aaf084461c1271110c151de
IveryChen <ivy_chen1@brown.edu>
2024-09-03 22:11:36 -0700

    removing loader

--- my-3d-site/src/components/MainPage/VideoLoader.jsx
+++ my-3d-site/src/components/MainPage/VideoLoader.jsx
@@ -5,54 +5,12 @@ import VideoPlayer from "./VideoPlayer";
 import "./VideoLoader.css";
 
 const VideoLoader = ({ videoUrls }) => {
-  const [loadingProgress, setLoadingProgress] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const totalVideos = videoUrls.length;
-  const loadedVideos = useRef(0);
-
-  useEffect(() => {
-    if (totalVideos === 0) {
-      setLoadingProgress(100);
-      setIsLoading(false);
-      return;
-    }
-
-    const handleLoadedData = () => {
-      loadedVideos.current += 1;
-      const progress = (loadedVideos.current / totalVideos) * 100;
-      setLoadingProgress(progress);
-
-      if (loadedVideos.current === totalVideos) {
-        setIsLoading(false);
-      }
-    };
-
-    const videoElements = Array.from(document.getElementsByClassName("video"));
-    videoElements.forEach((video) => {
-      video.addEventListener("loadeddata", handleLoadedData);
-    });
-
-    return () => {
-      videoElements.forEach((video) => {
-        video.removeEventListener("loadeddata", handleLoadedData);
-      });
-    };
-  }, [totalVideos]);
-
   return (
     <div className="video-progress">
-      {isLoading && (
-        <div className="progress-bar">
-          <div
-            className="progress"
-            style={{ width: `${loadingProgress}%` }}
-          ></div>
-        </div>
-      )}
-      <div
-        className="all-videos"
-        style={{ opacity: isLoading ? 0 : 1, transition: "opacity 0.3s ease" }}
-      >
+      <div className="progress-bar">
+        <div className="progress"></div>
+      </div>
+      <div className="all-videos">
         {map(videoUrls, (url) => (
           <VideoPlayer key={url} url={url} />
         ))}
