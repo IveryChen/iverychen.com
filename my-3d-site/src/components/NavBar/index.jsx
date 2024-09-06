8f1c5c10a0954fa5a69c0093abb51da26cdd4e0c
IveryChen <ivy_chen1@brown.edu>
2024-09-05 23:12:19 -0700

    add star

--- my-3d-site/src/components/NavBar/index.jsx
+++ my-3d-site/src/components/NavBar/index.jsx
@@ -52,7 +52,7 @@ const NavBar = () => {
     <header className="navbar">
       <Link to="/">
         <StyledLogo cv08 tnum>
-          IVERY CHEN
+          ✫ IVERY CHEN
         </StyledLogo>
       </Link>
       <div className="menu-toggle-container">
