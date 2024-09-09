182629e8412d2541ce751aa896842f5350f1f537
IveryChen <ivy_chen1@brown.edu>
2024-09-08 19:57:47 -0700

    change gridTemplateColumn

--- my-3d-site/src/components/MainPage/VideoLoader.jsx
+++ my-3d-site/src/components/MainPage/VideoLoader.jsx
@@ -4,26 +4,20 @@ import { map } from "lodash";
 import Box from "../Box";
 import VideoPlayer from "./VideoPlayer";
 
-const StyledBox = styled(Box)`
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
-  }
-`;
-
 const VideoLoader = ({ videoUrls }) => {
   return (
     <Box height="auto" overflowY="hidden" position="relative" width="100vw">
-      <StyledBox
+      <Box
         display="grid"
         gap="20px"
-        gridTemplateColumns="repeat(auto-fill, minmax(320px, 1fr))"
+        gridTemplateColumns="repeat(auto-fill, minmax(500px, 1fr))"
         overflow-y="hidden"
         padding="16px"
       >
         {map(videoUrls, (url) => (
           <VideoPlayer key={url} url={url} />
         ))}
-      </StyledBox>
+      </Box>
     </Box>
   );
 };
