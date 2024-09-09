615e049ef9dc3a901ccebbfaa133285cab4dbecd
IveryChen <ivy_chen1@brown.edu>
2024-09-08 22:46:12 -0700

    make responsive to mobile, still only expand in one spot

--- my-3d-site/src/components/MainPage/VideoPlayer.jsx
+++ my-3d-site/src/components/MainPage/VideoPlayer.jsx
@@ -1,26 +1,52 @@
+import styled from "@emotion/styled";
 import React from "react";
 
 import Box from "../Box";
+
+const StyledBox = styled(Box)`
+  position: relative;
+  display: grid;
+  transition: all 0.8s ease;
+
+  ${({ isActive }) =>
+    isActive &&
+    `
+      grid-column: 1 / 3;
+      grid-row: 1 / 3;
+    `}
+
+  @media (max-width: 1133px) {
+    ${({ isActive }) =>
+      isActive &&
+      `
+        grid-column: auto;
+        grid-row: auto;
+      `}
+  }
+`;
 
 export default class VideoPlayer extends React.PureComponent {
   render() {
     const { isActive, url, onClick } = this.props;
 
-    const boxStyle = isActive ? { gridColumn: "1 / 3", gridRow: "1 / 3" } : {};
+    const videoStyle = {
+      objectFit: "cover",
+    };
 
     return (
-      <Box display="grid" style={boxStyle}>
+      <StyledBox isActive={isActive}>
         <video
           autoPlay
+          height="100%"
           loop
           muted
           onClick={onClick}
           src={`https://d2skwsfewsc9s1.cloudfront.net/Videos/${url}.mp4`}
+          style={videoStyle}
           type="video/mp4"
           width="100%"
-          height="100%"
         />
-      </Box>
+      </StyledBox>
     );
   }
 }
