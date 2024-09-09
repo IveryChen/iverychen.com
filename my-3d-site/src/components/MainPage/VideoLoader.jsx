98116746eaa091bd6dc2da88426679ec11931a71
IveryChen <ivy_chen1@brown.edu>
2024-09-08 18:13:13 -0700

    removed VideoLoader css

--- my-3d-site/src/components/MainPage/VideoLoader.jsx
+++ my-3d-site/src/components/MainPage/VideoLoader.jsx
@@ -1,18 +1,30 @@
+import styled from "@emotion/styled";
 import { map } from "lodash";
-import React, { useState, useEffect, useRef } from "react";
 
+import Box from "../Box";
 import VideoPlayer from "./VideoPlayer";
-import "./VideoLoader.css";
+
+const StyledBox = styled(Box)`
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  }
+`;
 
 const VideoLoader = ({ videoUrls }) => {
   return (
-    <div className="video-progress">
-      <div className="all-videos">
+    <Box height="auto" overflowY="hidden" position="relative" width="100vw">
+      <StyledBox
+        display="grid"
+        gap="20px"
+        gridTemplateColumns="repeat(auto-fill, minmax(320px, 1fr))"
+        overflow-y="hidden"
+        padding="16px"
+      >
         {map(videoUrls, (url) => (
           <VideoPlayer key={url} url={url} />
         ))}
-      </div>
-    </div>
+      </StyledBox>
+    </Box>
   );
 };
 
